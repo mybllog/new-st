cf32523c4f02fd4c3d19e6113fb3ade5d5c190d9
mybllog <chisomeleanora@gmail.com>
2025-02-25 15:46:02 +0100

    added scrollable to the tab and a image to the services

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -80,6 +80,8 @@ export default function CenteredTabs() {
   <Tabs
     value={value}
     onChange={handleChange}
+    visibleScrollbar
+    variant="scrollable"
     centered
     textColor="black"
     indicatorColor="primary"
